34e29ddf2cd5613a88a949328e2a1fb1f3cd713f
Lucas <lucasdev2124@gmail.com>
2024-02-18 23:49:26 -0300

    auth page skeleton

---
+++ pages/auth.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-main class="h-screen w-screen pa-0 ma-0">
+        <v-container class="w-100 h-100 d-flex justify-center align-center">
+            <v-window class="w-75 h-100">
+                <v-window-item class="h-100 w-100">
+                    <v-card class="h-100 w-100 bg-yellow">
+                    
+                    </v-card>
+                </v-window-item>
+                <v-window-item class="h-100 w-100">
+                    <v-card class="h-100 w-75 bg-black">
+                    
+                    </v-card>
+                </v-window-item>
+            </v-window>
+        </v-container>
+
+    </v-main>
+</template>
