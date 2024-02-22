82c7dc2cbc76fda3f7141e1482dc5be2ab1dbb22
Lucas <lucasdev2124@gmail.com>
2024-02-21 23:04:52 -0300

    modularize stuff

---
+++ components/layout/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+        <v-card tag="footer" class="bg-teal-lighten-2">
+        <v-container class="d-flex flex-column py-6 ga-6">
+            <header class="text-h4">Life Cris</header>
+
+            <v-row class="flex-shrink-1">
+                <v-col cols='4'>
+                    <v-list class="bg-transparent">
+                        <v-list-item-title>daskdoasda</v-list-item-title>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                    </v-list>
+                </v-col>
+
+                <v-col cols='4'>
+                    <v-list class="bg-transparent">
+                        <v-list-item-title>daskdoasda</v-list-item-title>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                        <v-list-item class="mx-0 px-0">lalallalaa</v-list-item>
+                    </v-list>
+                </v-col>
+                <v-col cols='4'>
+                    <v-list class="bg-transparent mx-0">
+                        <v-list-item-title>Contato</v-list-item-title>
+                        <v-list-item class="mx-0 px-0">(21) 032190321931</v-list-item>
+                        <v-list-item class="mx-0 px-0">EsteticaLifecris@gmail.com</v-list-item>
+                    </v-list>
+                    <v-row>
+                        <v-list></v-list>
+                        <v-list-item-title>Redes sociais</v-list-item-title>
+                        <v-list-item clas="d-flex"><v-icon>mdi-instagram</v-icon><v-icon>mdi-instagram</v-icon><v-icon>mdi-instagram</v-icon></v-list-item>
+                    </v-row>
+                </v-col>
+            </v-row>
+            <footer class="d-flex flex-column justify-center align-center">
+                <h3>Estetica Life cris LTDA <br/> CNPJ 039201930192989</h3>
+                <NuxtLink class="text-left text-teal-darken-1  cursor-pointer font-italic font-weight-bold">Política de privacidade</NuxtLink>
+            </footer>
+        </v-container>
+        <v-footer class="w-100 bg-teal-darken-1 text-center pa-2">
+            <v-card-text class="pa-1">Desenvolvido por <NuxtLink class="text-teal-lighten-3 cursor-pointer font-italic font-weight-bold">DPOSKADPKASPD</NuxtLink></v-card-text>
+        </v-footer>
+    </v-card>
+</template>
