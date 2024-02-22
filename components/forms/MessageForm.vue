b160380ae493b9c963d6b08676b43ae1bffa4ccb
Lucas <lucasdev2124@gmail.com>
2024-02-21 23:05:34 -0300

    create components form

---
+++ components/forms/MessageForm.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-form class="pa-3">
+        <h3>Deixe sua avaliação</h3>
+        <v-rating size="small"></v-rating>
+        <v-textarea placeholder="escreva seu comentário aqui"></v-textarea>
+    </v-form>
+</template>
