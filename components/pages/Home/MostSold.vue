96198ee235656293dfcc904e4a928c15e1c0cc14
Lucas <lucasdev2124@gmail.com>
2024-02-23 23:42:34 -0300

    adding responsiveness to some components

--- components/pages/Home/MostSold.vue
+++ components/pages/Home/MostSold.vue
@@ -3,22 +3,21 @@
         <v-container class="w-100 h-100">
             <v-sheet class="h-100 w-100 bg-transparent d-flex flex-column justify-space-around">
                     <v-card-title class="text-h4 text-center text-wrap text-sm-h4 text-h5 font-weight-bold">Mais vendidos</v-card-title>
-                    <v-slide-group center-active class="h-75 d-flex justify-center align-center">                
+                    <v-slide-group center-active class="h-100 d-flex justify-center align-center">                
                         <v-slide-group-item v-for="n in 20" :key="n" >
-                            <v-card class="d-flex flex-column mx-3   bg-white rounded-lg" height="350px" width="200px">
-                                <img src="https://duohaus.com.br/wp-content/uploads/2021/07/massagemodeladora1.jpeg" width="100%" height="45%"/>
-                                <v-card-item class="d-flex flex-column ga-2">
-                                    <article class="flex-shrink-1">
-                                        <v-card-title class="text-wrap">drenagem é coisa de viado NÃO FAZ</v-card-title>
-                                        <v-card-subtitle  class="text-wrap">Passe melhor pelo pós cirurgico!</v-card-subtitle>
+                            <v-card class="d-flex flex-column ma-3 responsive-card-slide bg-white rounded-lg overflow-hidden">
+                                <img src="https://duohaus.com.br/wp-content/uploads/2021/07/massagemodeladora1.jpeg" width="100%" height="40%"/>
+                                <div class="d-flex flex-shrink-1 flex-column justify-space-between ga-2 overflow-hidden py-3">
+                                    <article class="d-flex flex-column flex-shrink-1 flex-grow-1 overflow-auto ">
+                                        <v-card-title class="text-wrap " >drenagem é coisa de viado NÃO FAZdrenagem é coisa de viado NÃO FAZ</v-card-title>
+                                        <v-card-subtitle  class="text-wrap overflow-none">Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!Passe melhor pelo pós cirurgico!</v-card-subtitle>
                                     </article>
-
-
-                                    <v-card-actions class="flex flex-wrap ga-2">
-                                        <v-btn variant="tonal"> Ver mais </v-btn>
-                                        <v-btn variant="tonal ma-0"> + Carrinho </v-btn>
+                                    <v-card-actions class="d-flex w-100 justify-space-between flex-wrap" :style="{width: '100%', height: 'fit-content'}">
+                                        <v-btn variant="tonal" class="text-subtitle-2"> Ver mais </v-btn>
+                                        <v-btn variant="tonal" class="text-subtitle-2"> + Carrinho </v-btn>
                                     </v-card-actions>
-                                </v-card-item>
+                                </div>
+
 
                             </v-card>
                         </v-slide-group-item>
@@ -28,5 +27,6 @@
                     </v-btn>
             </v-sheet>
         </v-container>
+
     </section>
 </template>
--- pages/cart.vue
+++ pages/cart.vue
@@ -8,15 +8,15 @@
                 </header>
                 
                 <div class="d-flex flex-column ga-4 overflow-auto my-3 w-100 flex-grow-1 flex-shrink-1" >
-                    <div class="h-75" v-for='i in 12'>
-                        <div class="d-flex justify-space-evenly py-2 px-1 flex-column flex-md-row align-center ga-4 flex-grow-1 flex-shrink-1 overflow-auto">
-                            <div class="d-flex w-100 h-100 justify-between">
-                                <v-img class="w-50 h-75" height="75%" src="https://duohaus.com.br/wp-content/uploads/2021/07/massagemodeladora1.jpeg"/>
-                                <v-card-item class=" w-50 pa-1 pa-sm-2 text-center">
-                                    <v-card-title class="text-sm-h6 text-subtitle-1 text-wrap">Item bonito</v-card-title>
+                    <div class="h-75 w-100" v-for='i in 12'>
+                        <div class="d-flex w-100 h-100 justify-space-evenly py-2 px-1 flex-column flex-md-row align-center ga-4 flex-grow-1 flex-shrink-1 overflow-auto">
+                            <div class="d-flex flex-shrink-1 flex-grow-1 h-100 w-100 justify-between overflow-hidden">
+                                <v-img class="w-50 h-100" cover src="https://duohaus.com.br/wp-content/uploads/2021/07/massagemodeladora1.jpeg"/>
+                                <article class=" w-50 pa-1 pa-sm-2 text-center overflow-hidden">
+                                    <v-card-title class="text-sm-h6 text-subtitle-1 word-wrap">Item bonito</v-card-title>
                                     <v-card-subtitle class="text-sm-subtitle-1 text-subtitle-2 text-wrap overflow-auto">esse item faz bonitezas</v-card-subtitle>
                                     <v-card-text class="pa-0 ma-0 text-body-1 text-wrap  text-sm-subtitle-2">R$ 500,00</v-card-text>
-                                </v-card-item>
+                                </article>
                             </div>
                             <div class="d-flex  flex-sm-row flex-column ga-2 align-center justify-space-between w-100">
                                 <div class="d-flex w-100 ga-2 justify-space-between align-center">
