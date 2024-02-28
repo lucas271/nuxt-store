398387aa17fc60000137ec852b1bc78320af7412
Lucas <lucasdev2124@gmail.com>
2024-02-27 23:58:53 -0300

    create Item filter component

--- components/shared/ItemsGrid.vue
+++ components/shared/ItemsGrid.vue
@@ -1,5 +1,9 @@
 <template>
         <v-row>
+            <v-col v-if="$slots.default">
+                <slot/>
+            </v-col>
+            
             <v-col cols="12" md="4" sm="6" v-for="i in 6">
                 <sharedItem/>
             </v-col>
