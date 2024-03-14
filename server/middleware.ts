dfe7e4e6966838fba50d3a711ecc140b63465e7e
Lucas <lucasdev2124@gmail.com>
2024-03-13 22:22:42 -0300

    add user auth

---
+++ server/middleware.ts
@@ -0,0 +1,13 @@
+import { serverSupabaseClient } from '#supabase/server'
+
+export default defineEventHandler(async (event) => {
+    const client = await (await serverSupabaseClient(event)).auth.getUser()
+
+    if(!client) throw {errors: 'usuario não autenticado'}
+    const url = getRequestURL(event)
+    if((event.method === 'POST' || event.method === 'DELETE' || event.method === 'PUT') && !client) throw {errors: 'unauthenticated'}
+    
+    if((url.pathname.includes('/product') || url.pathname.includes('/category')) && 
+    (event.method === 'DELETE' || event.method === 'PUT' || event.method === 'POST') &&
+    client.data.user?.role !== 'admin') throw {errors: "You're not an admin"}
+})
