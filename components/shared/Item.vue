33aa2934d6d6c1dc06dd3caaf013f21ebe0bf13e
Lucas <lucasdev2124@gmail.com>
2024-03-23 23:51:29 -0300

    fix reusability problems.

--- components/shared/Item.vue
+++ components/shared/Item.vue
@@ -2,7 +2,7 @@
     <v-card class="flex-column d-flex ga-2 bg-teal-lighten-2 rounded-lg w-100" height="600px">
         <v-img class="h-50 w-100 " cover :src="img ? img : 'https://duohaus.com.br/wp-content/uploads/2021/07/massagemodeladora1.jpeg'">
             <slot/>
-            <v-btn variant="" class="text-subtitle-2 text-red-darken-1 bg-red-lighten-3" icon="mdi-heart" density="comfortable" position="absolute" :style="{top: '5%', right: '5%'}"></v-btn>
+            <v-btn variant="" class="text-subtitle-2 text-blue-grey-darken-1 bg-blue-grey-lighten-3" icon="mdi-heart-outline" density="comfortable" position="absolute" :style="{top: '5%', right: '5%'}"></v-btn>
 
         </v-img>
         <div class='flex-grow-1 d-flex justify-space-around flex-column'>
@@ -13,9 +13,13 @@
                 <v-btn class="text-body-1"  variant="tonal">
                     Ver mais
                 </v-btn>
-                <v-btn class="text-body-1"  variant="tonal" >
-                    + Carrinho
-                    
+                <v-btn class="text-body-1 text-center"  variant="tonal" :disabled='cartStore.loading' @click='cartStore.addProduct(id)'>
+                    <template v-if='cartStore.loading'>
+                        <v-progress-circular indeterminate size="50"/>
+                    </template>
+                    <template v-else>
+                        + carrinho
+                    </template>
                 </v-btn>
             </v-card-actions>
         </div>
@@ -23,17 +27,22 @@
 </template>
 
 <script setup lang="ts">
+
+import {useCartStore} from '../../lib/services/cartStore.ts'
+
+const cartStore = useCartStore()
+
 interface ItemInterface {
     id: string,
     createdAt?: string,
     updatedAt?:string,
     name: string,
-    description: string,
+    description?: string,
     title: string
     price: number,
     img?: string,
-    quantity: number
-    is_available: boolean,
+    quantity?: number
+    is_available?: boolean,
     categoryName?: string
 }
 defineProps<ItemInterface>()
--- components/shared/ItemsGrid.vue
+++ components/shared/ItemsGrid.vue
@@ -1,11 +1,11 @@
 <template>
         <v-row class="flex-grow-1">
 
-            <template v-if="!productStore.loading">
+            <template v-if="!loading">
                 <v-col cols="12" md="4" sm="6" v-if="$slots.default">
                     <slot/>
                 </v-col>
-                <v-col cols="12" md="4" sm="6" v-for="product in productStore.products" height="500px">
+                <v-col cols="12" md="4" sm="6" v-for="product in products" height="500px">
                     <sharedItem :id="product.id" :name="product.name" :description="product.description" :img="product.img" :title="product.title" :price="product.price" :quantity="product.quantity" :is_available="product.is_available" >
                     
                     </sharedItem>
@@ -21,28 +21,29 @@
 </template>
 
 <script setup lang="ts">
-    import {useProductStore} from '../../lib/services/productStore.ts'
     interface productInterface {
-        id: string,
-        createdAt?: string,
-        updatedAt?:string,
-        name: string,
-        description: string,
-        title: string
-        price: number,
-        img?: string,
-        quantity: number
-        is_available: boolean,
-        category?: string,
+        products: {
+            id: string,
+            createdAt?: string,
+            updatedAt?:string,
+            name: string,
+            description: string,
+            title: string
+            price: number,
+            img?: string,
+            quantity: number
+            is_available: boolean,
+            category?: string,
+        }[]
     }
 
-    const productStore = useProductStore()
 
     const props = defineProps<{
         storeFunc: () => Promise<any>,
         watchVariable?: any,
         take: number,
         skip: number,
+        loading: boolean,
     } & productInterface>()
     const watchVariableProp = ref(props.watchVariable)
 
