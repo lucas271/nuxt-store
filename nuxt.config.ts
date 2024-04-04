764be7dd6794f181d3f9f0e54b5a4560e036b0aa
Lucas <lucasdev2124@gmail.com>
2024-04-03 22:39:06 -0300

    add csrf token to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,19 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@nuxtjs/google-fonts',
     '@nuxtjs/supabase',
+    "nuxt-csurf",
   ],
+  csurf: { 
+    https: false, 
+    cookie: { 
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict'
+    },
+    methodsToProtect: ['POST', 'PUT', 'PATCH'], // the request methods we want CSRF protection for
+    encryptAlgorithm: 'aes-256-cbc', // by default 'aes-256-cbc' (node), 'AES-CBC' (serverless)
+    addCsrfTokenToEventCtx: true // default false, to run useCsrfFetch on server set it to true
+  },
   supabase:{
     redirect: false
   },
