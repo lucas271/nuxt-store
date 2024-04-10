e0b24af30db3741c7420aafaafe30c7165cf98ec
Lucas <lucasdev2124@gmail.com>
2024-04-09 23:57:39 -0300

    increase Item component adaptability

--- components/shared/Item.vue
+++ components/shared/Item.vue
@@ -1,22 +1,22 @@
 <template>
-    <v-card class="flex-column d-flex ga-2 bg-teal-lighten-2 rounded-lg w-100" height="600px">
+    <v-card class="flex-column d-flex ga-2 bg-teal-lighten-2 rounded-lg w-100" :height="height ? height :'600px'">
         <v-img class="h-50 w-100 " cover :src="img ? img : 'https://duohaus.com.br/wp-content/uploads/2021/07/massagemodeladora1.jpeg'" :name='id'>
             <slot/>
         </v-img>
         <div class='flex-grow-1 d-flex justify-space-around flex-column'>
             <v-card-title class="font-italic">{{name}}</v-card-title>
             <v-card-subtitle class="text-wrap">{{title}}</v-card-subtitle>
-            <v-card-text>R${{ price.toFixed(2).replace('.', ',') || '' }}</v-card-text>
+            <v-card-text>R${{ price && price.toFixed(2).replace('.', ',') || '' }}</v-card-text>
             <v-card-actions >
-                <v-btn class="text-body-1"  variant="tonal">
+                <v-btn class="text-body-1"  variant="tonal" @click="navigateTo(`/item/${id}`)">
                     Ver mais
                 </v-btn>
-                <v-btn class="text-body-1 text-center"  variant="tonal" :disabled='cartStore.loading' @click='async () => await cartStore.addProduct(id)'>
-                    <template v-if='cartStore.loading'>
-                        <v-progress-circular indeterminate size="50"/>
+                <v-btn class="text-body-1 text-center"  variant="tonal" :disabled='cartStore.cartProducts.find(product => product.product.id === id)?.product?.loading' @click='async () => await cartStore.addProduct(id)'>
+                    <template v-if='cartStore.cartProducts.find(product => product.product.id === id)?.product?.loading'>
+                        <v-progress-circular indeterminate size="25"/>
                     </template>
                     <template v-else>
-                        + carrinho
+                        + Carinho
                     </template>
                 </v-btn>
             </v-card-actions>
@@ -25,15 +25,13 @@
 </template>
 
 <script setup lang="ts">
-
+import { useRouter } from 'vue-router';
 import {useCartStore} from '../../lib/services/cartStore.ts'
 import {useProductStore} from '../../lib/services/productStore.ts'
 
 const supabase = useSupabaseClient()
 const cartStore = useCartStore()
-const route = useRoute()
-
-const productStore = useProductStore()
+const route = useRouter()
 
 interface ItemInterface {
     id: string,
@@ -46,7 +44,9 @@ interface ItemInterface {
     img?: string,
     quantity?: number
     is_available?: boolean,
-    categoryName?: string
+    categoryName?: string,
+    loading?: boolean,
+    height?: string,
 }
 defineProps<ItemInterface>()
 </script>
