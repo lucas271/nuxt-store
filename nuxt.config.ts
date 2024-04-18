cc25b93e868a420e460ea766b8f02f372e9b0e93
Lucas <lucasdev2124@gmail.com>
2024-04-18 13:34:50 -0300

    trying to solve deploy csrf MISMATCH

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,11 +36,9 @@ export default defineNuxtConfig({
     cookie: { 
       path: '/',
       httpOnly: true,
-      sameSite: 'strict'
     },
     methodsToProtect: ['POST', 'PUT', 'PATCH'], // the request methods we want CSRF protection for
     encryptAlgorithm: 'AES-CBC', // by default '' (node), 'AES-CBC' (serverless)
-    addCsrfTokenToEventCtx: true // default false, to run useCsrfFetch on server set it to true
   },
   supabase:{
     redirect: false
